993d49d621fe6034f499eef55a404595fd4394da
Lilitr <liliananathaly1409@gmail.com>
2024-12-16 13:27:25 -0500

    Bases fundamentales

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+def main():
+    # Layout
+    # Main window
+    window = Tk()
+    
+
+
+    # Start the application
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
