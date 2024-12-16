4ad7e8e67cdff905fb9ca51e209151ac1abb8f02
Lilitr <liliananathaly1409@gmail.com>
2024-12-16 14:49:09 -0500

    Mejoras visuales y de formato de codigo

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     # Colors
     background = "#332f2f"
     buttons = "#878383"
+    active_button = "#bf9693"
     # Main window
     window = Tk()
     window.title("Calculadora")
@@ -14,8 +15,11 @@ def main():
 
     # Structure
     title = Label(
-        window, text="Mi Calculadora", font=("Arial", 30, "bold"),
-        bg=background, fg="white"
+        window,
+        text="Mi Calculadora",
+        font=("Arial", 30, "bold"),
+        bg=background,
+        fg="white",
     )  # Change this later!!
     title.grid(row=0, column=0, columnspan=4)
 
@@ -24,22 +28,168 @@ def main():
     expression.grid(row=1, column=0, columnspan=4)
 
     # Buttons
-    button_1 = Button(window, text=1, padx=36, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_2 = Button(window, text=2, padx=36, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_3 = Button(window, text=3, padx=36, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_4 = Button(window, text=4, padx=39, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_5 = Button(window, text=5, padx=36, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_6 = Button(window, text=6, padx=36, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_7 = Button(window, text=7, padx=36, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_8 = Button(window, text=8, padx=39, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_9 = Button(window, text=9, padx=36, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_0 = Button(window, text=0, padx=36, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_clear = Button(window, text="borrar", padx=69, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_plus = Button(window, text="+", padx=36, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_minus = Button(window, text="-", padx=36, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_multp = Button(window, text="*", padx=39, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_div = Button(window, text="/", padx=39, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
-    button_equal = Button(window, text="=", padx=173, pady=20, bg=buttons, fg="white", font=("Arial", 10 ,"bold"))
+    button_1 = Button(
+        window,
+        text=1,
+        padx=36,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_2 = Button(
+        window,
+        text=2,
+        padx=36,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_3 = Button(
+        window,
+        text=3,
+        padx=36,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_4 = Button(
+        window,
+        text=4,
+        padx=39,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_5 = Button(
+        window,
+        text=5,
+        padx=36,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_6 = Button(
+        window,
+        text=6,
+        padx=36,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_7 = Button(
+        window,
+        text=7,
+        padx=36,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_8 = Button(
+        window,
+        text=8,
+        padx=39,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_9 = Button(
+        window,
+        text=9,
+        padx=36,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_0 = Button(
+        window,
+        text=0,
+        padx=36,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_clear = Button(
+        window,
+        text="borrar",
+        padx=69,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+        activebackground=active_button,
+    )
+    button_plus = Button(
+        window,
+        text="+",
+        padx=36,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_minus = Button(
+        window,
+        text="-",
+        padx=36,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_multp = Button(
+        window,
+        text="*",
+        padx=39,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_div = Button(
+        window,
+        text="/",
+        padx=39,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+    )
+    button_equal = Button(
+        window,
+        text="=",
+        padx=173,
+        pady=20,
+        bg=buttons,
+        fg="white",
+        font=("Arial", 10, "bold"),
+        cursor="hand2",
+        activebackground=active_button,
+    )
 
     # Place the buttons
     button_1.grid(row=2, column=0)
